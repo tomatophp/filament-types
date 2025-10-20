36481a7758c9b072ff7c951bb695502a43a97b27
fadymondy <11937812+fadymondy@users.noreply.github.com>
2025-10-20 10:51:14 +0000

    Format Code

--- src/Pages/BaseTypePage.php
+++ src/Pages/BaseTypePage.php
@@ -6,8 +6,6 @@ use BackedEnum;
 use Filament\Actions\Action;
 use Filament\Forms\Components\ColorPicker;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Concerns\InteractsWithForms;
-use Filament\Forms\Contracts\HasForms;
 use Filament\Notifications\Notification;
 use Filament\Pages\Page;
 use Filament\Schemas\Components\Grid;
